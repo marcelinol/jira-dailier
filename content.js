8b2cc42521ba1430c0f8cd7508c658313b6770b9
marcelinol <marcelinolucianom@gmail.com>
2017-05-27 18:42:55 -0300

    Add show functionality

--- content.js
+++ content.js
@@ -3,12 +3,16 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      // Remove board header
-      document.getElementById('ghx-header').style.display = 'none';
-
-      // Remove navbar
-      document.getElementsByClassName('aui-header')[0].style.display = 'none';
-
+      var header = document.getElementById('ghx-header');
+      if(header.style.display === 'none') {
+        document.getElementById('ghx-header').style.display = '';
+        document.getElementsByClassName('aui-header')[0].style.display = '';
+      } else {
+        // Remove board header
+        document.getElementById('ghx-header').style.display = 'none';
+        // Remove navbar
+        document.getElementsByClassName('aui-header')[0].style.display = 'none';
+      }
     }
   }
 );
