6418c7a5b15f11f25a2a2b4f727552232461170c
Pedro Bachiega <pedro@pedrobachiega.com>
2017-06-12 16:12:06 -0300

    fixes

--- content.js
+++ content.js
@@ -1,12 +1,10 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var header = document.getElementById('ghx-header');
-      var navbar = document.getElementsByClassName('aui-header')[0];
+      toggleVisibility(document.getElementById('ghx-header'));
+      toggleVisibility(document.getElementsByClassName('aui-header')[0]);
 
-      toggleVisibility(header);
-      toggleVisibility(navbar);
-
+      adjustHeightForWindow(document.getElementById('ghx-work'));
       adjustHeightForWindow(document.getElementById('ghx-plan'));
       adjustHeightForWindow(document.getElementById('ghx-plan-group'));
     }
@@ -19,9 +17,11 @@ var toggleVisibility = function(element) {
   } else {
     element.style.display = 'none';
   }
-}
+};
 
 var adjustHeightForWindow = function(element) {
-  element.style.height = window.innerHeight + "px";
+  if (element !== undefined && element !== null) {
+    element.style.height = window.innerHeight + "px";
+  }
 }
 
